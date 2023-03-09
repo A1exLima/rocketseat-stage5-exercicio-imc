514ac76076d90aaa06bc1340f812f188749700f7
Alima07 <alex.lima_dev@outlook.com>
2023-03-09 10:41:47 -0300

    Estruturação de dados

--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -13,83 +13,86 @@ let textImc = document.querySelector(".modal-title")
 let textInterpretation = document.querySelector(".modal-text")
 
 //Events
-buttonClose.addEventListener("click", reloadpage)
+buttonClose.onclick = () => screen.toogleScreen()
 
-//Submit
+//Function Submit
 form.onsubmit = (event) => {
   event.preventDefault(event)
 
   let weight = weightInput.value
   let height = heightInput.value
 
-  let imc = calculateImc(weight, height)
-  let classificationImc = interpretationImc(imc)
+  let imc = logicImc.calculateImc(weight, height)
+  let classificationImc = logicImc.interpretationImc(imc)
 
-  textImc.innerHTML = `Seu IMC é de ${imc}`
-  textInterpretation.innerHTML = classificationImc
+  textImc.innerText = `Seu IMC é de ${imc}`
+  textInterpretation.innerText = classificationImc
 
-  checkError(imc, classificationImc)
+  screen.checkError(imc, classificationImc)
 }
 
-function calculateImc(weight, height) {
-  let imc = weight / (height / 100) ** 2 // IMC = Peso ÷ Altura² => obs:(altura em metros)
-  imc = imc.toFixed(1)
-  return imc
+//functions - Data structuring
+const logicImc = {
+  calculateImc(weight, height) {
+    return (weight / (height / 100) ** 2).toFixed(1) // IMC = Peso ÷ Altura² => obs:(altura em metros)
+  },
+  interpretationImc(imc) {
+    //Classificacao IMC
+    let skinny,
+      normal,
+      overweight,
+      obesityOne,
+      obesityTwo,
+      severeObesity = false
+
+    skinny = imc < 18.5 //magreza
+    normal = imc >= 18.5 && imc <= 24.9 //normal
+    overweight = imc >= 25 && imc <= 29.9 //sobrepeso
+    obesityOne = imc >= 30 && imc <= 34.9 //obesidade I
+    obesityTwo = imc >= 35 && imc <= 39.9 // Obesidade II
+    severeObesity = imc > 40 // obesidade grave
+
+    switch (true) {
+      case skinny:
+        return "Peso Baixo"
+        break
+      case normal:
+        return "Peso normal ou adequado"
+        break
+      case overweight:
+        return "Sobrepeso"
+        break
+      case obesityOne:
+        return "Obesidade Grau I"
+        break
+      case obesityTwo:
+        return "Obesidade Grau II"
+        break
+      case severeObesity:
+        return "Obesidade Grau III ou Mórbida"
+      default:
+        return "NULL"
+        break
+    }
+  },
 }
 
-function interpretationImc(imc) {
-  //Classificacao IMC
-  let skinny,
-    normal,
-    overweight,
-    obesityOne,
-    obesityTwo,
-    severeObesity = false
+const screen = {
+  toogleScreen() {
+    screenResult.classList.toggle("modal-hide")
+  },
+  checkError(imc, classificationImc) {
+    if (imc == "NaN" || classificationImc == "NULL") {
+      screenError.innerText = "Digite somente números"
+      screenError.classList.add("hide-alert")
 
-  skinny = imc < 18.5 //magreza
-  normal = imc >= 18.5 && imc <= 24.9 //normal
-  overweight = imc >= 25 && imc <= 29.9 //sobrepeso
-  obesityOne = imc >= 30 && imc <= 34.9 //obesidade I
-  obesityTwo = imc >= 35 && imc <= 39.9 // Obesidade II
-  severeObesity = imc > 40 // obesidade grave
+    } else if (imc == "Infinity" || imc == 0) {
+      screenError.innerText = "Digite o peso e a altura"
+      screenError.classList.add("hide-alert")
 
-  switch (true) {
-    case skinny:
-      return "Peso Baixo"
-      break
-    case normal:
-      return "Peso normal ou adequado"
-      break
-    case overweight:
-      return "Sobrepeso"
-      break
-    case obesityOne:
-      return "Obesidade Grau I"
-      break
-    case obesityTwo:
-      return "Obesidade Grau II"
-      break
-    case severeObesity:
-      return "Obesidade Grau III ou Mórbida"
-    default:
-      return "NULL"
-      break
-  }
-}
-
-function toogleScreen() {
-  screenResult.classList.toggle("modal-hide")
-}
-
-function reloadpage() {
-  toogleScreen()
-}
-
-function checkError(imc, classificationImc) {
-  if (imc == "NaN" && classificationImc == "NULL") {
-    screenError.classList.add("hide-alert")
-  } else {
-    screenError.classList.remove("hide-alert")
-    toogleScreen()
-  }
+    } else {
+      screenError.classList.remove("hide-alert")
+      screen.toogleScreen()
+    }
+  },
 }
